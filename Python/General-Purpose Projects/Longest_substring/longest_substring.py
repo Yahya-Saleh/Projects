bf915599f1af578b16a14f984bb449f6d43ff561
Yahya Saleh <g.o.yahya.sherif@gmail.com>
2021-02-08 18:19:07 +0300

    updated longest_string code

--- Python/General-Purpose Projects/Longest_substring/longest_substring.py
+++ Python/General-Purpose Projects/Longest_substring/longest_substring.py
@@ -1,21 +1,3 @@
-"""
-Assume s is a string of lower case characters.
-
-Write a program that prints the longest substring of s in which the letters occur in alphabetical order.
-For example, if s = 'azcbobobegghakl', then your program should print
-
-Longest substring in alphabetical order is: beggh
-
-In the case of ties, print the first substring. For example, if s = 'abcbcd', then your program should
-print
-
-Longest substring in alphabetical order is: abc
-
-Note: This problem may be challenging. We encourage you to work smart. If you've spent more than a few
-hours on this problem, we suggest that you move on to a different part of the course. If you have time,
-come back to this problem after you've had a break and cleared your head.
-"""
-
 s = input("Enter a sentence: ")
 
 longest_string = ""
@@ -39,5 +21,4 @@ while i < len(s) - 1:
     # Go to the next letter
     i += 1
 
-# print("Longest substring in alphabetical order is: " + str(longest_string))
 print(f"Longest substring in alphabetical order is: {longest_string}")
